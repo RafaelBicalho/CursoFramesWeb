0c0f8d8e172e55ac4f8d41d9cbf24de927b440f6
RafaelBicalho <rafaelbicalho@gmail.com>
2017-10-03 19:59:58 -0300

    nao grava deb cred
    nao grava deb cred

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -12,8 +12,9 @@
 
         vm.refresh = function(){
             $http.get(url).then(function(response){
-                vm.billingCycle = {}
+                vm.billingCycle = {credits:[{}], debts:[{}]}
                 vm.billingCycles = response.data
+                vm.calculateValues()
                 tabs.show(vm, {tabList: true, tabCreate: true })
             })
         }
@@ -29,11 +30,24 @@
 
         vm.showTabUpdate = function(billingCycle){
               vm.billingCycle = billingCycle
+               initCreditsAndDebts()
+              vm.calculateValues()
               tabs.show( vm, {tabUpdate:true})
           }
             vm.showTabDelete = function(billingCycle){
                   vm.billingCycle = billingCycle
+                  vm.calculateValues()
                   tabs.show( vm, {tabDelete:true})
+              }
+
+              vm.update = function() {
+                const updateUrl = `${url}/${vm.billingCycle._id}`
+                $http.put(updateUrl,vm.billingCycle).then(function(response) {
+                  vm.refresh()
+                  msgs.addSuccess('Operacao realizada com sucesso!.')
+                }).catch(function(response) {
+                  msgs.addError(response.errors)
+                })
               }
 
               vm.delete = function() {
@@ -41,11 +55,71 @@
                 $http.delete(deleteUrl,vm.billingCycle).then(function(response) {
                   vm.refresh()
                   msgs.addSuccess('Operacao realizada com sucesso!.')
-                }).catch(function(data) {
-                  msgs.addError(data.errors)
+                }).catch(function(response) {
+                  msgs.addError(response.errors)
                 })
               }
 
+
+              vm.addCredit = function(index) {
+                vm.billingCycle.credits.splice(index + 1, 0, {})
+              }
+
+              vm.cloneCredit = function(index  ,{ name, value} ) {
+                vm.billingCycle.credits.splice(index + 1, 0, {name, value })
+                vm.calculateValues()
+              }
+
+              vm.deleteCredit = function(index)  {
+                if( vm.billingCycle.credits.length > 1) {
+                vm.billingCycle.credits.splice(index , 1 )
+                vm.calculateValues()
+                  }
+              }
+
+
+              vm.addDebt = function(index) {
+                vm.billingCycle.debts.splice(index + 1, 0, {})
+              }
+
+              vm.cloneDebt = function(index  ,{ name, value} ) {
+                vm.billingCycle.debts.splice(index + 1, 0, {name, value })
+                vm.calculateValues()
+              }
+
+              vm.deleteDebt = function(index)  {
+                if( vm.billingCycle.debts.length > 1) {
+                vm.billingCycle.debts.splice(index , 1 )
+                vm.calculateValues()
+                  }
+              }
+
+              vm.calculateValues = function() {
+                vm.credit = 0
+                vm.debt   = 0
+                if (vm.billingCycle) {
+                  vm.billingCycle.credits.forEach(function({value}) {
+                    vm.credit += !value || isNaN(value) ? 0 : parseFloat(value)
+                  })
+                  vm.billingCycle.debts.forEach(function({value}) {
+                    vm.debt += !value || isNaN(value) ? 0 : parseFloat(value)
+                  })
+                }
+                vm.total = vm.credit - vm.debt
+              }
+
+
+                var initCreditsAndDebts = function() {
+                  if(!vm.billingCycle.debts || !vm.billingCycle.debts.length) {
+                    vm.billingCycle.debts = []
+                    vm.billingCycle.debts.push({})
+                  }
+
+                  if(!vm.billingCycle.credits || !vm.billingCycle.credits.length) {
+                    vm.billingCycle.credits = []
+                    vm.billingCycle.credits.push({})
+                  }
+                   }
         vm.refresh()
     }
 })()
