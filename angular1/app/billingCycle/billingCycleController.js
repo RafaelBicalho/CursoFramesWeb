0903c1d3a1c20e3d7560dceb261978d2c4333fcf
RafaelBicalho <rafaelbicalho@gmail.com>
2017-10-18 00:41:00 -0200

    AAAA
    AAAA

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -11,6 +11,7 @@
             $http.get(url).then(function(response){
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
                 vm.billingCycle = {}
                 vm.billingCycles = response.data
 =======
@@ -22,6 +23,10 @@
                 vm.billingCycle = {}
                 vm.billingCycles = response.data
 >>>>>>> parent of 76ac509... Merge pull request #2 from RafaelBicalho/v090817
+=======
+                vm.billingCycle = {}
+                vm.billingCycles = response.data
+>>>>>>> parent of 0c0f8d8... nao grava deb cred
                 tabs.show(vm, {tabList: true, tabCreate: true })
             })
         }
@@ -45,16 +50,20 @@
               vm.billingCycle = billingCycle
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
                initCreditsAndDebts()
               vm.calculateValues()
 >>>>>>> parent of e98c815... sdsaaddas
 =======
 >>>>>>> parent of 76ac509... Merge pull request #2 from RafaelBicalho/v090817
+=======
+>>>>>>> parent of 0c0f8d8... nao grava deb cred
               tabs.show( vm, {tabUpdate:true})
           }
             vm.showTabDelete = function(billingCycle){
                   vm.billingCycle = billingCycle
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
                   tabs.show( vm, {tabDelete:true})
@@ -81,11 +90,17 @@
               }
 
 >>>>>>> parent of 76ac509... Merge pull request #2 from RafaelBicalho/v090817
+=======
+                  tabs.show( vm, {tabDelete:true})
+              }
+
+>>>>>>> parent of 0c0f8d8... nao grava deb cred
               vm.delete = function() {
                 const deleteUrl = `${url}/${vm.billingCycle._id}`
                 $http.delete(deleteUrl,vm.billingCycle).then(function(response) {
                   vm.refresh()
                   msgs.addSuccess('Operacao realizada com sucesso!.')
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
                 }).catch(function(data) {
@@ -167,6 +182,13 @@
               }
 
 >>>>>>> parent of 76ac509... Merge pull request #2 from RafaelBicalho/v090817
+=======
+                }).catch(function(data) {
+                  msgs.addError(data.errors)
+                })
+              }
+
+>>>>>>> parent of 0c0f8d8... nao grava deb cred
         vm.refresh()
 =======
 >>>>>>> parent of afe1fbb... Merge pull request #1 from RafaelBicalho/v090817
